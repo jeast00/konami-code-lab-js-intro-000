e47a444af0a1ff1c831c40d46048996803bbff29
Jonathan East <jonathan_east@ymail.com>
2020-03-22 23:59:53 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -17,20 +17,8 @@ function init() {
   // your code here
   document.addEventListener("keydown", (event) => {
 
-  var key = event.key;
+  var key = codes[event.key];
   console.log(key);
 
-  var codeIndex = codes[index];
-
-  if(key == codeIndex) {
-    index++;
-
-    if(index == codes.length) {
-      alert("Hurray!");
-      index = 0;
-    }
-  } else {
-    index = 0;
-  }
   });
 }
